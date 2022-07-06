bf1224fad87644fdb33bc9030ae6c43ac60cc5d7
Marcos-mi <marcosminof22@gmail.com>
2022-07-05 21:22:32 -0300

    Nuevos ejercicios

---
+++ stock.php
@@ -0,0 +1,26 @@
+<?php 
+
+$stock = 800;
+
+    if($stock > 0){
+        echo "Hay stock";
+    }else{
+        echo "No hay stock";
+    }
+
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Stock</title>
+</head>
+<body>
+    
+</body>
+</html>
