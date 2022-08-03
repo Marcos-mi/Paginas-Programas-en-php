6085d5b3822f76e1b4a5e499a620eaa1b296a694
Marcos-mi <marcosminof22@gmail.com>
2022-08-03 15:21:09 -0300

    datos

--- print_f.php
+++ print_f.php
@@ -6,17 +6,23 @@ error_reporting(E_ALL);
 
 function print_f($variable){
     if(is_array($variable)){
+        $archivo = fopen("datos.txt", "a+");
         //si es un array lo recorro y guardo el contenido en el archivo "datos.txt
+        foreach($variable as $item){
+            fwrite($archivo, $item . "\n");
+        }
+        fclose($archivo);
     }else{
-        //entonces si es strin, guardo el contenido en el archivo "datos.txt"
+        //entonces si es string, guardo el contenido en el archivo "datos.txt"
         file_put_contents("datos.txt", $variable);
     }
+    echo "archivo generado";
 }
 
 //uso
-$aNotas = array(8,5,7,9,10);
-$msg = "este es un mensaje";
-print_f($aNotas);
+$aNotas = array(8,5,7,9,10,1,2,6,5,3,2,6);
+$msg = "este es un mensaje! Nos estaremos comunicando contigo a la brevedad";
+print_f($msg);
 
 
 
